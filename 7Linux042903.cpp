3f8f21df662b9c652b888d520a05df56354c3142
luckily777 <253974227@qq.com>
2020-05-02 16:28:43 +0800

    操作linux和打印进度得操作

---
+++ 7Linux042903.cpp
@@ -0,0 +1,29 @@
+[root@luckily ~]# cd study
+[root@luckily study]# ls
+day01  day02  day03
+[root@luckily study]# cd day03
+[root@luckily day03]# ls
+process  process.c
+[root@luckily day03]# vim process.c
+[root@luckily day03]# vim Jindutiao.c
+[root@luckily day03]# make Jindutiao
+cc     Jindutiao.c - o Jindutiao
+[root@luckily day03]# . / Jindutiao
+[#][root@luckily day03]#
+[root@luckily day03]# vim Jindutiao.c
+[root@luckily day03]# make Jindutiao
+cc     Jindutiao.c - o Jindutiao
+[root@luckily day03]# . / j
+bash : . / j : 没有那个文件或目录
+[root@luckily day03]# . / Jindutiao
+[#                                                                                     [##[###[####[#####[######[#######[########[#########[##########[###########[############ ^ [[#############[############## ^ C]
+[root@luckily day03]# vim Jindutiao.c
+[root@luckily day03]# make Jindutiao
+cc     Jindutiao.c - o Jindutiao
+[root@luckily day03]# . / Jindutiao
+^ C####################################################################################### ]]
+[root@luckily day03]# vim Jindutiao.c
+[root@luckily day03]# make Jindutiao
+cc     Jindutiao.c - o Jindutiao
+[root@luckily day03]# . / Jindutiao
+[#                                                                                     [##[###[####[#####[######[#######[########[#########[##########[###########[################################[#################################[##################################[###################################[####################################[##################################### ^ C[######################################[#######################################[######################################## ^ C][40 %][\]
