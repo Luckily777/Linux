3fcaae1a0f647dd4169bf3a54469d97a0c42ad56
Luckily777 <253974227@qq.com>
2020-05-03 17:12:26 +0800

    设置行号

---
+++ SetNum.c
@@ -0,0 +1,10 @@
+#设置行号
+[root@luckily ~]# cd /etc
+[root@luckily etc]# ls
+[root@luckily etc]# vim vimrc
+[root@luckily etc]# cd
+[root@luckily ~]# cd study
+[root@luckily study]# cd day03
+[root@luckily day03]# vim process.c
+[root@luckily day03]# vim SetNum.c
+
