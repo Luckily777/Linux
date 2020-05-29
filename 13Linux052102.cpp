e21c4bd30227f4041ad9c5d066415cf164a2bf02
luckily777 <253974227@qq.com>
2020-05-29 23:11:13 +0800

    1

---
+++ 13Linux052102.cpp
@@ -0,0 +1,25 @@
+[root@luckily ~]# cd study
+[root@luckily study]# cls
+bash : cls: Î´ÕÒµ½ÃüÁî...
+[root@luckily study]# ls
+day01  day02  day03  day04  day05  day06  day07  day08
+[root@luckily study]# mkdir day09
+[root@luckily study]# cd day09
+[root@luckily day09]# vim 01.c
+[root@luckily day09]# echo "abcdefghijklmnopqrstuvwxyz" > test.txt
+[root@luckily day09]# vim test.txt
+[root@luckily day09]# vim 01.c
+[root@luckily day09]# gcc 01.c
+[root@luckily day09]# . / a.out
+buf1 = [abcdefghij]
+buf2 = [abcdefghij]
+[root@luckily day09]# vim 01.c
+[root@luckily day09]# gcc 01.c
+[root@luckily day09]# . / a.out
+buf1 = [abcdefghij]
+buf2 = [abcdefghij]
+buf1 = [AAAAAp]
+[root@luckily day09]# vim 01.c
+[root@luckily day09]# vim 01.c
+[root@luckily day09]# 
+
