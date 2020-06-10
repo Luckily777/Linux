0e3d193388a084a8c9a7358ac0560761b784207a
luckily777 <253974227@qq.com>
2020-06-10 21:29:15 +0800

    俄罗斯和pwd操作

---
+++ 16Linux060301.cpp
@@ -0,0 +1,38 @@
+//俄罗斯方块
+Luckilyuckily day0c]# printf \033[33m "hello"
+0Luckilyroot@luckily day0c]# printf "\033[33mhello"
+heLuckilyt@luckily day0c]# ls
+01.Luckily.2.c  01.c  02.c  03.c  04.c  05.c  a.out  mykill
+[rooLuckilyly day0c]# printf "\033[33mhello\033[0m"
+helloLuckilyuckily day0c]# 
+[root@Luckily day0c]# printf "\033[10;3H\033[33mhello\033[0m"
+Luckily
+Luckily[root@luckily day0c]# 
+[root@luckily day0c]# ay0c]# 
+[root@luckily day0c]# vim tetris.c
+[root@luckily day0c]# gcc tetris.c
+[root@luckily day0c]# . / a.out
+[root@luckily day0c]# vim tetris.c
+[root@luckily day0c]# echo $pwd
+
+
+
+//自己版本的echo的pwd需要$ 后加大写的PWD
+
+[root@luckily day0c]# pwd
+/ root / study / day0c
+[root@luckily day0c]# echo "$pwd"
+
+[root@luckily day0c]# echo "pwd"
+pwd
+[root@luckily day0c]# echo 'pwd'
+pwd
+[root@luckily day0c]# echo $pwd
+
+[root@luckily day0c]# pwd
+/ root / study / day0c
+[root@luckily day0c]# echo 'pwd'
+pwd
+[root@luckily day0c]# echo $PWD
+/ root / study / day0c
+[root@luckily day0c]# 
