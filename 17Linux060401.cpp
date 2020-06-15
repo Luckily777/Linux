620e23a16afcbf782e799141d50f2447cd8d3cbf
luckily777 <253974227@qq.com>
2020-06-15 23:33:27 +0800

    0

---
+++ 17Linux060401.cpp
@@ -0,0 +1,21 @@
+[][][][][][][][][][] study
+[][][][][][][][][][]# ls
+[][][][][][][][][][] day04  day05  day06  day07  day08  day09  day0a  day0b  day0c
+[][][][][][][][][][]# cd day0c
+[][][][][][][][][][]# ls
+[][][][][][][][][][]05.c  a.out       keyboard.h      mykill
+[][][][][][][root@luckily day0c]# .c  libkeyboard.so  tetris.c
+[root@luckily day0c]# vim tetris.c
+[root@luckily day0c]# gcc tetris.c - L. - lkeyboard - I.
+[root@luckily day0c]# _line’中:
+[root@luckily day0c] # 隐式声明与内建函数‘memcpy’不兼容[默认启用]
+[root@luckily day0c]# [k], background[k - 1], sizeof(background[i]));
+[root@luckily day0c] # 
+[root@luckily day0c]# 隐式声明与内建函数‘memset’不兼容[默认启用]
+[root@luckily day0c]# 0], 0x00, sizeof(background[0]));
+[root@luckily day0c] # 
+[root@luckily day0c]# vim tetris.c
+[root@luckily day0c]# gcc tetris.c - L. - lkeyboard - I.
+[root@luckily day0c]# . / a.out
+[root@luckily day0c]# 
+[root@luckily day0c]# 
